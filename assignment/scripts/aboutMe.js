9a06ad886beeca33d6e0ef031178c614c423a553
Lizzie Q <lizzieqtech@gmail.com>
2022-02-02 17:29:27 -0600

    WK2PT1-AssignmentCorrected

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -11,8 +11,10 @@ console.log(fullName);
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 let luckyNumber = 10;
 // 6 - Console log this sentence, adding in the variables you created above: //NOTE: actual variable name is NOT a string as variable value is a string:
-console.log ('My name is '  + fullName   + ', and I think ' + luckyNumber +  ' is a winner!');
-// Refer back to the videos if you need help with this one.
+//console.log ('My name is '  + fullName   + ', and I think ' + luckyNumber +  ' is a winner!');
+// Refer back to the videos if you need help with this one. USE TEMPLATE LITERALS:
+console.log(`My name is ${fullName}, and I think, ${luckyNumber}, is a winner!`);
+
 
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
 let adventurous = false;
@@ -26,16 +28,23 @@ let friendsPets = 2;
 friendsPets += 2;  // cant do just +   , must do += as in itself Plus value!
 
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-const allowedPets = 3;
+const allowedPets = 2;
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!",
 // if it's not true, console log "How about we stay home?"
 /*if (conditional){
  code if true ;
 }else {    };  OR multiple    else if    before the else*/
-if (adventurous = false) {
+//If (adventurous = true) {
+//console.log("Adventures are great!");
+//}else{
+ //   console.log("How about we stay home?");
+//}
+//or - since adventurous boolean already defined as false would change boolean this way to true:
+adventurous = true;
+if (adventurous) {
     console.log("Adventures are great!");
 }else{
-    console.log("How about we stay home?");
+   console.log("How about we stay home?");
 }
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
@@ -50,8 +59,10 @@ if (luckyNumber === 2 && adventurous === true) {
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
-if ( pets >= allowedPets ){
-    console.log("I can have more pets");  
+
+pets = 1;
+if ( pets < allowedPets ){
+    console.log("I can have more pets!");  
 }else if (pets === allowedPets){
     console.log("I have enough pets");
 }else {
